58734c64470f0c64feb022e6fe2cd25d2f1bab48
Tuấn Vũ <113121915+John0209@users.noreply.github.com>
2022-11-17 18:51:43 +0700

    Vu Do OrderDetail

---
+++ DataAcces/Repository/IOrderDetailRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.Models;
+namespace DataAccess.Repository
+{
+    public interface IOrderDetailRepository
+    {
+        IEnumerable<OrderDetail> GetOrderDetails();
+        OrderDetail GetOrderDetailByID(int OrderID, int ProductID);
+        void InsertOrderDetail(OrderDetail OrderDetail);
+        void DeleteOrderDetail(int OrderID, int ProductID);
+        void UpdateOrderDetail(OrderDetail OrderDetail);
+
+    }
+}
---
+++ DataAcces/Repository/OrderDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.Models;
+namespace DataAccess.Repository
+{
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        public IEnumerable<OrderDetail> GetOrderDetails() => OrderDetailDAO.Instance.GetOrderDetailList();
+        public OrderDetail GetOrderDetailByID(int OrderID, int ProductID) => OrderDetailDAO.Instance.GetOrderDetailByID(OrderID, ProductID);
+        public void InsertOrderDetail(OrderDetail OrderDetail) => OrderDetailDAO.Instance.AddNew(OrderDetail);
+        public void DeleteOrderDetail(int OrderID, int ProductID) => OrderDetailDAO.Instance.Remove(OrderID, ProductID);
+        public void UpdateOrderDetail(OrderDetail OrderDetail) => OrderDetailDAO.Instance.Update(OrderDetail);
+
+      
+    }
+}
