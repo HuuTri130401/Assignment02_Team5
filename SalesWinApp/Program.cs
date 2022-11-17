665d2190630d346768f716282801e15b8edbf005
Baokhanhfpt <Khanhndbse151327@fpt.edu.vn>
2022-11-17 17:06:21 +0700

    add MemberDAO, MemberRepository, IMemberRepository, frmLogin, Nuget

--- SalesWinApp/Program.cs
+++ SalesWinApp/Program.cs
@@ -1,3 +1,5 @@
+using MyStoreWinApp;
+
 namespace SalesWinApp
 {
     internal static class Program
--- SalesWinApp/frmLogin.cs
+++ SalesWinApp/frmLogin.cs
@@ -1,7 +1,17 @@
-namespace SalesWinApp
+﻿using DataAccess;
+using DataAccess.Repository;
+using BusinessObject.Models;
+using Nancy.Json;
+using SalesWinApp;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MyStoreWinApp
 {
     public partial class frmLogin : Form
     {
+        private MemberRepository memberRepository = new MemberRepository();
         public frmLogin()
         {
             InitializeComponent();
@@ -12,10 +22,72 @@ namespace SalesWinApp
 
         }
 
-        private void frmOrders_Click(object sender, EventArgs e)
+        private void btnLog_Click(object sender, EventArgs e)
         {
-            frmOrders frmOrders = new frmOrders();
-            frmOrders.ShowDialog();
+            string json = string.Empty;
+
+            // read json string from file
+            using (StreamReader reader = new StreamReader("appsettings.json"))
+            {
+                json = reader.ReadToEnd();
+            }
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            // convert json string to dynamic type
+            var obj = jss.Deserialize<dynamic>(json);
+
+            // get contents
+            string Email = obj["DefaultAccount"]["Email"];
+            string Password = obj["DefaultAccount"]["password"];
+            bool isMem = false;
+
+            if (Email.Equals(txtUserName.Text) && Password.Equals(txtPassword.Text))
+            {
+                frmMain frm = new frmMain()
+                {
+                    isAdmin = true
+                };
+                frm.ShowDialog();
+                isMem = true;
+
+                this.Close();
+
+            }
+
+            // add employees to richtextbox
+
+            var members = memberRepository.GetMembers();
+
+            foreach (var i in members)
+            {
+                if (i.Email.Equals(txtUserName.Text) && i.Password.Equals(txtPassword.Text))
+                {
+                    frmMain frm = new frmMain()
+                    {
+                        isAdmin = false
+                    };
+                    frm.ShowDialog();
+                    isMem = true;
+
+                    this.Close();
+                    break;
+
+                }
+                
+            }
+            if (isMem == true)
+            {
+                MessageBox.Show("Login Success", "Right user");
+            }
+            else
+            {
+                MessageBox.Show("Wrong user name or pass word, please try again", "Wrong user");
+
+            }
         }
+
+        private void btnCancel_Click(object sender, EventArgs e) => this.Close();
+
     }
 }
